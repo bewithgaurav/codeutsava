34fe10451ce32461dd09e1ed59a794753dbf2614
Gaurav <gauravsharma2696@gmail.com>
2017-01-03 13:53:58 +0530

    Final Push with changes

--- application.py
+++ application.py
@@ -5,36 +5,61 @@ from datetime import *
 from flask_mail import Mail, Message
 import hashlib
 from datetime import datetime
+import string
+import random
 
 db = MySQLdb.connect("localhost","root","toor","libmanage")
 cursor = db.cursor()
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
-
+# app.secret_key = os.urandom(24)
+app.secret_key = 'lAltCodeUtsvaGaurAvMayaNk'
 # +----+--------+-------+----------+--------+--------+---------+--------+------+------+------------+---------+-------+------+------+--------+--------+
 # | id | active | email | teamname | resume | github | contact | gender | dob  | yop  | experience | address | state | city | name | degree | stream |
 # +----+--------+-------+----------+--------+--------+---------+--------+------+------+------------+---------+-------+------+------+--------+--------+
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'gauravandsanskar@gmail.com'
-app.config['MAIL_PASSWORD'] = '8602229193'
+# app.config['MAIL_SERVER']='smtp.gmail.com'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'gauravandsanskar@gmail.com'
+# app.config['MAIL_PASSWORD'] = '8602229193'
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_SERVER']='smtp.sendgrid.net'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = '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'
 app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_DEBUG']= True
 mail = Mail(app)
+
+def some_random_string(size=8, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+@app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session.pop('_csrf_token', None)
+        if not token or token != request.form.get('_csrf_token'):
+            abort(403)
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = some_random_string()
+        print(session['_csrf_token'])
+    return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
+
 
 @app.route('/',methods=['GET', 'POST'])
 def home():
-    if 'team' in session:
-        return redirect(url_for('teamprofile'))
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/registration',methods=['GET', 'POST'])
 def index():
     if 'team' in session:
         return redirect(url_for('teamprofile'))
-    return render_template("index.html")
+    return render_template("registerindex.html")
 
 @app.route('/logout')
 def logout():
@@ -43,7 +68,7 @@ def logout():
         session.clear()
         return render_template("login.html",teamname=teamname)
     session.clear()
-    return render_template("index.html")
+    return redirect(url_for('index'))
 
 @app.route('/activate',methods=['GET', 'POST'])
 def activate():
@@ -51,25 +76,25 @@ def activate():
     hashstring=request.args.get('hash')
     
     # If email ALready Exists
-    query="select teamname from members where email='%s' and active=%s"%(email,'1')
-    cursor.execute(query)
+    query="select teamname from members where email=%s and active=%s"
+    cursor.execute(query,[email,'1'])
     d=cursor.fetchone()
     if d:
         return render_template("activated.html",exist=1,teamname=d[0])
     
     #For displaying Teamname and Password
-    query="select teamname from members where email='%s'"%(email)
-    cursor.execute(query)
+    query="select teamname from members where email=%s"
+    cursor.execute(query,[email])
     d=cursor.fetchone()
     teamname=d[0]
-    query="select password from teams where teamname='%s'"%(teamname)
-    cursor.execute(query)
+    query="select password from teams where teamname=%s"
+    cursor.execute(query,[teamname])
     d=cursor.fetchone()
     password=d[0]
 
     #Matching the hash received with the one from database
-    query="select id from members where email='%s'"%(email)
-    cursor.execute(query)
+    query="select id from members where email=%s"
+    cursor.execute(query,[email])
     id=cursor.fetchone()
     id=str(id[0])
     reverse=str(id[0])+email
@@ -79,13 +104,13 @@ def activate():
         print("yes equal")
         try:
             # print("INSERT INTO emails VALUES ('%s')"%(email))
-            cursor.execute("INSERT INTO emails VALUES ('%s')"%(email))
+            cursor.execute("INSERT INTO emails VALUES (%s)",[email])
             db.commit()
             print("Email added to the mailing List")
         except:
             db.rollback()
         try:
-            cursor.execute("""update members set active=%s where id=%s""",('1',id))
+            cursor.execute("""update members set active=%s where id=%s""",['1',id])
             db.commit()
             return render_template("activated.html",value=True,teamname=teamname,password=password)
         except:
@@ -107,13 +132,14 @@ def showall():
 @app.route('/viewteam',methods=['GET','POST'])
 def viewteam():
     name=str(request.args.get('team'))
-    q="select name,degree,stream from members where teamname='%s'"%name
-    cursor.execute(q)
+    q="select name,degree,stream from members where teamname=%s"
+    cursor.execute(q,[name])
     d=cursor.fetchall()
     print(d)
-    q="select active from teams where teamname='%s'"%(name)
-    cursor.execute(q)
+    q="select active,institute from teams where teamname=%s"
+    cursor.execute(q,[name])
     teamstatus=cursor.fetchone()
+    institute=teamstatus[1]
     teamstatus=teamstatus[0]
     if d:
         c=[]
@@ -122,7 +148,7 @@ def viewteam():
         print(c)
     else:
         return("Wrong Team Name")
-    return render_template('viewteam.html',data=c,teamname=name,teamstatus=teamstatus)
+    return render_template('viewteam.html',data=c,teamname=name,teamstatus=teamstatus,institute=institute)
 
 @app.route('/resend',methods=['GET'])
 def resend():
@@ -130,12 +156,12 @@ def resend():
         return("You Need to Login first")
     resendemail=request.args.get('email')
     try:
-        query="select id from members where email='%s'"%(resendemail)
-        cursor.execute(query)
+        query="select id from members where email=%s"
+        cursor.execute(query,[resendemail])
         id1=cursor.fetchone()
         id1=str(id1[0])+resendemail
         f=str(hashlib.md5(id1.encode('utf-8')).hexdigest())
-        msgstring='Click here to confirm the registration for your team '+session['team']+'- '+'http://localhost:5000/activate?email='+resendemail+'&hash='+f 
+        msgstring='Click here to confirm the registration for your team '+session['team']+'- '+'http://codeutsava.in/activate?email='+resendemail+'&hash='+f 
         msg = Message("Team Registration Successful", sender = 'gauravandsanskar@gmail.com', recipients = [resendemail])
         msg.body = msgstring
         mail.send(msg)
@@ -154,6 +180,7 @@ def memprofile():
         name=request.form.get('name','')
         gender=request.form.get('gender','')
         github=request.form.get('github','')
+        twitter=request.form.get('twitter','')
         resume=request.form.get('resume','')
         yop=request.form.get('yop','')
         contact=request.form.get('contact','')
@@ -176,17 +203,17 @@ def memprofile():
             # print(name,gender,github,resume,yop,contact,experience,address,degree,stream,city,state,dob,session["currentmember"])
 
             # print("UPDATE members set status=%d,name='%s',gender='%s',github='%s',resume='%s',yop='%s',contact='%s',experience='%s',address='%s',degree='%s',stream='%s',city='%s',state='%s',dob='%s'  where email='%s'"%(1,name,gender,github,resume,yop,contact,experience,address,degree,stream,city,state,dob,session["currentmember"]))
-            cursor.execute("""UPDATE members set status=%s,name=%s,gender=%s,github=%s,resume=%s,yop=%s,contact=%s,experience=%s,address=%s,degree=%s,stream=%s,city=%s,state=%s,dob=%s where email=%s""",('1',name,gender,github,resume,yop,contact,experience,address,degree,stream,city,state,dob,session["currentmember"]))
+            cursor.execute("""UPDATE members set status=%s,name=%s,gender=%s,github=%s,resume=%s,yop=%s,contact=%s,experience=%s,address=%s,degree=%s,stream=%s,city=%s,state=%s,dob=%s,twitter=%s where email=%s""",['1',name,gender,github,resume,yop,contact,experience,address,degree,stream,city,state,dob,twitter,session["currentmember"]])
             # cursor.execute("""update members set status=%s where email=%s""",('1',session["currentmember"]))
             db.commit()
             flash('Successfully Saved')
         except:
             db.rollback()
-            flash('Error Occurred')
+            flash('Error Occurred while Updating the Database. Please Try Again ')
     if request.args.get('email'):
         session['currentmember']=request.args.get('email')
-    query="select status,active from members where email='%s'"%(session['currentmember'])
-    cursor.execute(query)
+    query="select status,active from members where email=%s"
+    cursor.execute(query,[session['currentmember']])
     data=cursor.fetchone()
     data1=data[0]
     active=data[1]
@@ -196,8 +223,8 @@ def memprofile():
         return redirect(url_for("teamprofile"))
     if data:
         decision='Edit/View'
-        query="select * from members where email='%s'"%(session['currentmember'])
-        cursor.execute(query)
+        query="select * from members where email=%s"
+        cursor.execute(query,[session['currentmember']])
         d=cursor.fetchone()
     else:
         decision="Add"
@@ -208,6 +235,8 @@ def memprofile():
 
 @app.route('/register',methods=['GET', 'POST'])
 def register():
+    if 'team' in session:
+        return redirect(url_for('teamprofile'))
     if request.method=="POST":
         uname=request.form.get('uname','')
         password=request.form.get('password','')
@@ -215,15 +244,16 @@ def register():
         email2=request.form.get('email2','')
         email3=request.form.get('email3','')
         institute=request.form.get('institute','')
+        print(request.form.get('_csrf_token',''))
         if uname and password and email1 and email2 and email3 and institute:
-            query="select * from members where email='%s' and active=%s"%(email1,'1')
-            cursor.execute(query)
+            query="select * from members where email=%s and active=%s"
+            cursor.execute(query,[email1,'1'])
             data1=cursor.fetchone()
-            query="select * from members where email='%s' and active=%s"%(email2,'1')
-            cursor.execute(query)
+            query="select * from members where email=%s and active=%s"
+            cursor.execute(query,[email2,'1'])
             data2=cursor.fetchone()
-            query="select * from members where email='%s' and active=%s"%(email3,'1')
-            cursor.execute(query)
+            query="select * from members where email=%s and active=%s"
+            cursor.execute(query,[email3,'1'])
             data3=cursor.fetchone()
             if data1 or data2 or data3:
                 if data1:
@@ -233,48 +263,48 @@ def register():
                 if data3:
                     flash("Email ID "+email3+" Already Exists")
                 return render_template("register.html")
-            query="select * from teams where teamname='%s'"%(uname)
-            cursor.execute(query)
+            query="select * from teams where teamname=%s"
+            cursor.execute(query,[uname])
             data=cursor.fetchone()
             if data:
                 flash("Team Already Exists")
             else:
                 try:
-                    cursor.execute("""INSERT INTO teams (teamname, password, institute) VALUES (%s,%s,%s)""",(uname,password,institute))
-                    cursor.execute("""INSERT INTO members (teamname, email) VALUES (%s,%s)""",(uname,email1))
-                    cursor.execute("""INSERT INTO members (teamname, email) VALUES (%s,%s)""",(uname,email2))
-                    cursor.execute("""INSERT INTO members (teamname, email) VALUES (%s,%s)""",(uname,email3))
+                    cursor.execute("""INSERT INTO teams (teamname, password, institute) VALUES (%s,%s,%s)""",[uname,password,institute])
+                    cursor.execute("""INSERT INTO members (teamname, email) VALUES (%s,%s)""",[uname,email1])
+                    cursor.execute("""INSERT INTO members (teamname, email) VALUES (%s,%s)""",[uname,email2])
+                    cursor.execute("""INSERT INTO members (teamname, email) VALUES (%s,%s)""",[uname,email3])
                     try:
-                        query="select id from members where email='%s'"%(email1)
-                        cursor.execute(query)
+                        query="select id from members where email=%s"
+                        cursor.execute(query,[email1])
                         id1=cursor.fetchone()
                         id1=str(id1[0])+email1
                         print("tatti",id1)
                         f=str(hashlib.md5(id1.encode('utf-8')).hexdigest())
                         # print(f)
-                        msgstring='Click here to confirm the registration for your team '+uname+'- '+'http://localhost:5000/activate?email='+email1+'&hash='+f 
+                        msgstring='Click here to confirm the registration for your team '+uname+'- '+'http://codeutsava.in/activate?email='+email1+'&hash='+f 
                         # print("Hello")
                         
                         msg = Message("Team Registration Successful", sender = 'gauravandsanskar@gmail.com', recipients = [email1])
                         msg.body = msgstring
                         mail.send(msg)
                         
-                        query="select id from members where email='%s'"%(email2)
-                        cursor.execute(query)
+                        query="select id from members where email=%s"
+                        cursor.execute(query,[email2])
                         id2=cursor.fetchone()
                         id2=str(id2[0])+email2
                         f=(hashlib.md5(id2.encode('utf-8')).hexdigest())
-                        msgstring='Click here to confirm the registration for your team '+uname+'- '+'http://localhost:5000/activate?email='+email2+'&hash='+f 
+                        msgstring='Click here to confirm the registration for your team '+uname+'- '+'http://codeutsava.in/activate?email='+email2+'&hash='+f 
                         msg = Message("Team Registration Successful", sender = 'gauravandsanskar@gmail.com', recipients = [email2])
                         msg.body = msgstring
                         mail.send(msg)
                         
-                        query="select id from members where email='%s'"%(email3)
-                        cursor.execute(query)
+                        query="select id from members where email=%s"
+                        cursor.execute(query,[email3])
                         id3=cursor.fetchone()
                         id3=str(id3[0])+email3
                         f=(hashlib.md5(id3.encode('utf-8')).hexdigest())
-                        msgstring='Click here to confirm the registration for your team '+uname+'- '+'http://localhost:5000/activate?email='+email3+'&hash='+f 
+                        msgstring='Click here to confirm the registration for your team '+uname+'- '+'http://codeutsava.in/activate?email='+email3+'&hash='+f 
                         msg = Message("Team Registration Successful", sender = 'gauravandsanskar@gmail.com', recipients = [email3])
                         msg.body = msgstring
                         mail.send(msg)
@@ -296,9 +326,9 @@ def register():
 def teamprofile():
     if 'team' in session:
         status="Incomplete"
-        teamname=session['team']
-        query="select * from members where teamname='%s'"%(teamname)
-        cursor.execute(query)
+        teamname=str(session['team'])
+        query="select * from members where teamname=%s"
+        cursor.execute(query,[teamname])
         data=cursor.fetchall()
         # print("tatti",dat)
         mem1=data[0]
@@ -307,8 +337,8 @@ def teamprofile():
         active1=mem1[1]
         active2=mem2[1]
         active3=mem3[1]
-        query="select * from teams where teamname='%s'"%(teamname)
-        cursor.execute(query)
+        query="select * from teams where teamname=%s"
+        cursor.execute(query,[teamname])
         d=cursor.fetchone()
         institute=d[4]
         d=d[2]
@@ -320,7 +350,7 @@ def teamprofile():
             s3=mem3[17]
             if s1==1 and s2==1 and s3==1:
                 try:
-                    cursor.execute("""update teams set active=%s where teamname=%s""",('1',teamname))
+                    cursor.execute("""update teams set active=%s where teamname=%s""",['1',teamname])
                     status="Pending for Judgement"
                     db.commit()
                 except:
@@ -328,7 +358,7 @@ def teamprofile():
                     flash('Error Occurred')
 
         return render_template("teamprofile.html",data=data,teamname=teamname,status=status,institute=institute)
-    return ("You need to Login First")
+    return render_template("thanks.html",msg="You need to Login First")
 
 
 @app.route('/deleteteam',methods=['GET', 'POST'])
@@ -336,12 +366,12 @@ def deleteteam():
     if 'team' in session:
         teamname=session['team']
         try:
-            query="select * from members where teamname='%s'"%(teamname)
-            cursor.execute(query)
+            query="select * from members where teamname=%s"
+            cursor.execute(query,[teamname])
             data=cursor.fetchall()
             print(data)
-            cursor.execute("delete from members where teamname='%s'"%(teamname))
-            cursor.execute("delete from teams where teamname='%s'"%(teamname))
+            cursor.execute("delete from members where teamname=%s",[teamname])
+            cursor.execute("delete from teams where teamname=%s",[teamname])
             db.commit()
             session['deleted']=1
             # print("tatti",dat)
@@ -378,8 +408,8 @@ def login():
         uname=request.form.get('uname','')
         password=request.form.get('password','')
         if uname and password:
-            query="select * from teams where teamname='%s' and password='%s'"%(uname,password)
-            cursor.execute(query)
+            query="select * from teams where teamname=%s and password=%s"
+            cursor.execute(query,[uname,password])
             data=cursor.fetchone()
             if data:
                 session['team']=uname
@@ -388,7 +418,7 @@ def login():
                 flash("Invalid Credentials") 
     return render_template("login.html")
 
-
+# Jim Moriarty. Hi. Thanks for Checking out the code.
 
 if __name__ == '__main__':
     app.run( debug=True )
--- static/js/init.js
+++ static/js/init.js
@@ -1,8 +1,21 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $(".button-collapse").sideNav({
+      menuWidth: 200, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true,
+	  draggable:true// Closes side-nav on <a> clicks, useful for Angular/Meteor
+    // Choose whether you can drag to open on touch screens
+    }
+	 
+  );
+  $('.collapsible').collapsible();
+  $('.scrollspy').scrollSpy();
     $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+  
+    
