a21e6b30a351ac592264631eb93c5d0432107633
Gaurav <gauravsharma2696@gmail.com>
2017-01-05 04:21:31 +0530

    New push

--- static/js/init.js
+++ static/js/init.js
@@ -1,9 +1,9 @@
 (function($){
   $(function(){
 
-    $(".button-collapse").sideNav({
+    $(" .button-collapse").sideNav({
       menuWidth: 200, // Default is 240
-      edge: 'right', // Choose the horizontal origin
+      edge: 'left', // Choose the horizontal origin
       closeOnClick: true,
 	  draggable:true// Closes side-nav on <a> clicks, useful for Angular/Meteor
     // Choose whether you can drag to open on touch screens
